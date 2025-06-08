02633c7735a6816b298bf20144159982e60d69a3
MRRollMan <konkow@ukr.net>
2025-06-09 00:25:13 +0300

    Refactor bet button logic to include user balance for dynamic betting options

--- src/utils/game_messages.py
+++ src/utils/game_messages.py
@@ -6,8 +6,7 @@ from src.utils import TextBuilder, get_bet_buttons
 
 
 def get_basketball_message(chat_user, from_user):
-    tb, kb = TextBuilder(), InlineKeyboardBuilder()
-    kb.row(*get_bet_buttons(from_user.id, Games.BASKETBALL), width=2)
+    tb, kb = TextBuilder(), get_bet_buttons(from_user.id, Games.BASKETBALL, chat_user[3])
     tb.add("🏀 {user} якщо не виграєш трахну твою маму\nВибери ставку\n\n🏷️ У тебе: {balance} кг\n",
            user=TextMention(from_user.first_name, user=from_user),
            balance=Code(chat_user[3]))
@@ -16,8 +15,7 @@ def get_basketball_message(chat_user, from_user):
 
 
 def get_bowling_message(chat_user, from_user):
-    tb, kb = TextBuilder(), InlineKeyboardBuilder()
-    kb.row(*get_bet_buttons(from_user.id, Games.BOWLING), width=2)
+    tb, kb = TextBuilder(), get_bet_buttons(from_user.id, Games.BOWLING, chat_user[3])
     tb.add("🎳 {user} а тебе випадково в дитинстві не кидали?\nВибери ставку\n\n🏷️ У тебе: {balance} кг\n",
            user=TextMention(from_user.first_name, user=from_user),
            balance=Code(chat_user[3]))
@@ -26,8 +24,7 @@ def get_bowling_message(chat_user, from_user):
 
 
 def get_casino_message(chat_user, from_user):
-    tb, kb = TextBuilder(), InlineKeyboardBuilder()
-    kb.row(*get_bet_buttons(from_user.id, Games.CASINO), width=2)
+    tb, kb = TextBuilder(), get_bet_buttons(from_user.id, Games.CASINO, chat_user[3])
     tb.add("🎰 {user} шо ти, лудоман спідозний\nВибери ставку\n\n🏷️ У тебе: {balance} кг\n",
            user=TextMention(from_user.first_name, user=from_user),
            balance=Code(chat_user[3]))
@@ -36,8 +33,7 @@ def get_casino_message(chat_user, from_user):
 
 
 def get_darts_message(chat_user, from_user):
-    tb, kb = TextBuilder(), InlineKeyboardBuilder()
-    kb.row(*get_bet_buttons(from_user.id, Games.DARTS), width=2)
+    tb, kb = TextBuilder(), get_bet_buttons(from_user.id, Games.DARTS, chat_user[3])
     tb.add("🎯 {user}сідай на пляшку\nВибери ставку\n\n🏷️ У тебе: {balance} кг\n",
            user=TextMention(from_user.first_name, user=from_user),
            balance=Code(chat_user[3]))
@@ -46,8 +42,7 @@ def get_darts_message(chat_user, from_user):
 
 
 def get_dice_message(chat_user, from_user):
-    tb, kb = TextBuilder(), InlineKeyboardBuilder()
-    kb.row(*get_bet_buttons(from_user.id, Games.DICE), width=2)
+    tb, kb = TextBuilder(), get_bet_buttons(from_user.id, Games.DICE, chat_user[3])
     tb.add("🎲 {user}, якщо програєш,\nто заплатиш адміну через /shop\nВибери ставку\n\n🏷️ У тебе: {balance} кг\n",
            user=TextMention(from_user.first_name, user=from_user),
            balance=Code(chat_user[3]))
@@ -56,8 +51,7 @@ def get_dice_message(chat_user, from_user):
 
 
 def get_football_message(chat_user, from_user):
-    tb, kb = TextBuilder(), InlineKeyboardBuilder()
-    kb.row(*get_bet_buttons(from_user.id, Games.FOOTBALL), width=2)
+    tb, kb = TextBuilder(), get_bet_buttons(from_user.id, Games.FOOTBALL, chat_user[3])
     tb.add("⚽ {user} я знав що ти гей\nВибери ставку\n\n🏷️ У тебе: {balance} кг\n",
            user=TextMention(from_user.first_name, user=from_user),
            balance=Code(chat_user[3]))
@@ -66,8 +60,7 @@ def get_football_message(chat_user, from_user):
 
 
 def get_game_message(chat_user, from_user):
-    tb, kb = TextBuilder(), InlineKeyboardBuilder()
-    kb.row(*get_bet_buttons(from_user.id, Games.GAME), width=2)
+    tb, kb = TextBuilder(), get_bet_buttons(from_user.id, Games.GAME, chat_user[3])
     tb.add("🧟‍♂️ {user} бля дивлюся на тебе і ригати хочеться\nВибери ставку\n\n🏷️ У тебе: {balance} кг\n",
            user=TextMention(from_user.first_name, user=from_user),
            balance=Code(chat_user[3]))
